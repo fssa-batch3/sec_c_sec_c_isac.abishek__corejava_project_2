88468a30b903df8453b4bfce292f7da45ef6eba5
isac <116252006+isacdevaabishek@users.noreply.github.com>
2023-08-24 13:36:49 +0530

    Add files via upload

--- src/main/java/com/fssa/charitytrust/connection/ConnectionUtil.java
+++ src/main/java/com/fssa/charitytrust/connection/ConnectionUtil.java
@@ -22,13 +22,13 @@ public class ConnectionUtil {
 		String userName;
 		String passWord;
 		
-//		url = System.getenv("DATABASE_HOST");
-//		userName = System.getenv("DATABASE_USERNAME");
-//		passWord = System.getenv("DATABASE_PASSWORD");
+		url = System.getenv("DATABASE_HOST");
+		userName = System.getenv("DATABASE_USERNAME");
+		passWord = System.getenv("DATABASE_PASSWORD");
 
-		url = "jdbc:mysql://localhost:3306/charitytrust";
-		userName = "root";
-		passWord = "123456";
+//		url = "jdbc:mysql://localhost:3306/charitytrust";
+//		userName = "root";
+//		passWord = "123456";
 
 		try {
 			
